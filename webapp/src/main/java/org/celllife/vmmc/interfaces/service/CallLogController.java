c9d60dc6918827458868d7a1bc434712c7c497f4
dine <poekie1>
2014-05-02 13:45:35 +0200

    INTERNAL-556 IVR Project: Implement Reports

--- webapp/src/main/java/org/celllife/vmmc/interfaces/service/CallLogController.java
+++ webapp/src/main/java/org/celllife/vmmc/interfaces/service/CallLogController.java
@@ -1,24 +1,32 @@
 package org.celllife.vmmc.interfaces.service;
 
 import org.celllife.ivr.application.calllog.CallLogService;
+import org.celllife.ivr.application.utils.JsonUtils;
 import org.celllife.ivr.domain.callog.CallLog;
+import org.json.JSONException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class CallLogController {
 
+    private static Logger log = LoggerFactory.getLogger(CallLogController.class);
+
     @Autowired
     CallLogService callLogService;
 
+    JsonUtils jsonUtils = new JsonUtils();
+
     @Value("${external.base.url}")
-   String externalBaseUrl;
+    String externalBaseUrl;
 
     @ResponseBody
     @RequestMapping(value = "/verboice-service/serverCallback", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -59,11 +67,24 @@ public class CallLogController {
 
     }
 
-    @ResponseBody
-    @RequestMapping(value = "/verboice-service/passwordCallback", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public void updateCallLog(@RequestParam(value = "passwordEntered", required = true) String passwordEntered, @RequestParam(value = "verboiceProjectId", required = true) Integer verboiceProjectId, @RequestParam(value = "messageNumber", required = true) Integer messageNumber, @RequestParam(value = "msisdn", required = true) String msisdn) {
+    @RequestMapping(value = "/verboice-service/passwordCallback", method = RequestMethod.POST)
+    public void updateCallLog(@RequestBody String requestBody) {
 
-        CallLog callLog = callLogService.findCallLogByVerboiceIdAndMsisdnAndMessageNumber(verboiceProjectId,msisdn,messageNumber);
+        log.debug("Received password update from Verboice: " + requestBody);
+
+        Map<String, String> responseVariables = new HashMap<>();
+        try {
+            responseVariables = jsonUtils.extractJsonVariables("{\"response\":" + requestBody + "}");
+        } catch (JSONException e) {
+            log.warn("Could not extract JSON variables from Verboice response " + requestBody);
+        }
+
+        String passwordEntered = responseVariables.get("passwordEntered");
+        Integer verboiceProjectId = Integer.valueOf(responseVariables.get("verboiceProjectId"));
+        Integer messageNumber = Integer.valueOf(responseVariables.get("messageNumber"));
+        String msisdn = responseVariables.get("msisdn");
+
+        CallLog callLog = callLogService.findCallLogByVerboiceIdAndMsisdnAndMessageNumber(verboiceProjectId, msisdn, messageNumber);
 
         if ((callLog != null)) {
             callLog.setPasswordEntered(passwordEntered);
